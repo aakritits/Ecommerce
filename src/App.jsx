a8c3bba19b57c117aa45b1c59c9b18b08289fe17
Aakriti <aakritiad01@gmail.com>
2024-05-03 12:21:09 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Shop from "./Shop/Shop";
 import "./styles/design.css";
 import Navbar from "./components/Navbar";
-// import Category from "./components/Category";
 const App = () => {
   return (
     <div>
       <Router>
         <Navbar />
-        {/* <Category /> */}
         <Routes>
           <Route path="/" element={<Shop />} />
         </Routes>
